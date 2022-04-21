994ccec81c2f659a701512da6585712b4972b707
RasanYo <rasanhy@gmail.com>
2022-04-21 18:41:58 +0200

    fonction setup

--- Raspberry pi/main.py
+++ Raspberry pi/main.py
@@ -52,12 +52,15 @@ def decode_command(string, cntrl):
 
 
 def setup():
+    #Tableau d'instances qui communiquent avec les arduino (1 par arduino dans @devices)
     serials = [serial.Serial(port=name, baudrate=9600, timeout=1) for name in devices]
     index = -1
 
     while index == -1:
         for i, ser in enumerate(serials):
             ser.write("CHECK")
+
+            #Si on recoit une reponse on envoie un msg a l'arduino correspondante et on sort de la boucle
             if ser.readline().decode('utf-8').rstrip() is None:
                 index = i
                 ser.write("LINKED")
