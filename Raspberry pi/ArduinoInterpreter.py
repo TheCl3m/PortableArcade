64b67871a6df0ffddeb36b703b6b143441b82f5a
RasanYo <rasanhy@gmail.com>
2022-04-30 18:40:11 +0200

    nouveau main

--- Raspberry pi/ArduinoInterpreter.py
+++ Raspberry pi/ArduinoInterpreter.py
@@ -1,39 +1,88 @@
-import serial
-from evdev import UInput, AbsInfo, ecodes as e
 
-cap = {  e.EV_KEY : [e.KEY_0, e.KEY_1, e.KEY_2, e.KEY_3],
- e.EV_ABS : [ (e.ABS_X, AbsInfo(value=0, min=-512, max=512, 
-fuzz=0, flat=0, resolution=0)),
-(e.ABS_Y, AbsInfo(0, -512, 512, 0, 0, 0)),
- (e.ABS_MT_POSITION_X, (0, 128, 255, 0)) ]}
+import serial
+from evdev import _uinput, ecodes as e
+from evdev.device import AbsInfo
+
+cap = {e.EV_KEY: [e.KEY_0, e.KEY_1, e.KEY_2, e.KEY_3],
+       e.EV_ABS: [(e.ABS_X, AbsInfo(0, -512, 512,
+                                    0, 0, 0)),
+                  (e.ABS_Y, AbsInfo(0, -512, 512, 0, 0, 0)),
+                  (e.ABS_MT_POSITION_X, (0, 128, 255, 0))]}
+
+devices = ['/dev/ttyACM0', '/dev/ttyACM1']
 
 
 def create_controller():
+    return _uinput(cap, "VirtualController", version=0x1)
 
-    return UInput(cap, "VirtualController", version=0x1);
+
+def decode_command(string, cntrl):
+    cmd = string.split("-")
+
+    if cmd[0] == "BTN":
+
+        if cmd[1] == "A:ON":
+            cntrl.write(e.EV_KEY, e.KEY_0, 1)
+        else:
+            cntrl.write(e.EV_KEY, e.KEY_0, 0)
+
+        if cmd[1] == "B:ON":
+            cntrl.write(e.EV_KEY, e.KEY_1, 1)
+        else:
+            cntrl.write(e.EV_KEY, e.KEY_1, 0)
+
+        if cmd[1] == "C:ON":
+            cntrl.write(e.EV_KEY, e.KEY_2, 1)
+        else:
+            cntrl.write(e.EV_KEY, e.KEY_2, 0)
+
+        if cmd[1] == "D:ON":
+            cntrl.write(e.EV_KEY, e.KEY_3, 1)
+        else:
+            cntrl.write(e.EV_KEY, e.KEY_3, 0)
 
 
-def decode_command(string):
-    command = string.split("-")
-    match (command[0]):
-        case "BTN":
-            return command[1]
+        #if cmd[1] == "L3:ON":
+         #   cntrl.write(e.EV_KEY, e.KEY_4, 1)
+        #else:
+         #   cntrl.write(e.EV_KEY, e.KEY_4, 0)
 
-        case "JST":
-            coord = command[1].split(";")
-            return coord[0], coord[1]
 
-        case _:
-            return "Not recognized"
+
+
+    elif cmd[0] == "JST":
+        coord = cmd[1].split(";")
+        cntrl.write(e.EV_ABS, e.ABS_X, coord[0])
+        cntrl.write(e.EV_ABS, e.ABS_Y, coord[1])
+
+    return cmd
+
+
+def setup():
+
+    while True:
+        for name in devices:
+            try:
+                srl = serial.Serial(port=name, baudrate=9600, timeout=1)
+                srl.write("CHECK")
+
+                # Si on recoit une reponse on envoie un msg a l'arduino correspondante et on sort de la boucle
+                if srl.readline().decode('utf-8').rstrip() is None:
+                    ser.write("LINKED")
+                    return srl
+            except:
+                continue
 
 
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyACM0', 9600, 1)
+    print("Code started\n")
+    ser = setup()
+    print("Found serial " + ser)
     ser.flush()
-
     controller = create_controller()
-
+    print("controller created")
     while True:
         if ser.in_waiting > 0:
             command = ser.readline().decode('utf-8').rstrip()
-            print(decode_command(command))
+            print(decode_command(command, cntrl=controller))
+
--- Raspberry pi/main.py
+++ Raspberry pi/main.py
@@ -1,17 +1,22 @@
 import serial
-from evdev import uinput, AbsInfo, ecodes as e
+from uinput import Device
+import uinput
 
-cap = {e.EV_KEY: [e.KEY_0, e.KEY_1, e.KEY_2, e.KEY_3],
-       e.EV_ABS: [(e.ABS_X, AbsInfo(0, -512, 512,
-                                    0, 0, 0)),
-                  (e.ABS_Y, AbsInfo(0, -512, 512, 0, 0, 0)),
-                  (e.ABS_MT_POSITION_X, (0, 128, 255, 0))]}
+events = (
+    uinput.BTN_A,
+    uinput.BTN_B,
+    uinput.BTN_X,
+    uinput.BTN_Y,
+    uinput.BTN_JOYSTICK,
+    uinput.ABS_X + (-512, 512, 0, 0),
+    uinput.ABS_Y + (-512, 512, 0, 0),
+)
 
 devices = ['/dev/ttyACM0', '/dev/ttyACM1']
 
 
 def create_controller():
-    return uinput(cap, "VirtualController", version=0x1)
+    return Device(events)
 
 
 def decode_command(string, cntrl):
@@ -20,38 +25,38 @@ def decode_command(string, cntrl):
     if cmd[0] == "BTN":
 
         if cmd[1] == "A:ON":
-            cntrl.write(e.EV_KEY, e.KEY_0, 1)
+            cntrl.emit(uinput.BTN_A, 1)
         else:
-            cntrl.write(e.EV_KEY, e.KEY_0, 0)
+            cntrl.emit(uinput.BTN_A, 0)
 
         if cmd[1] == "B:ON":
-            cntrl.write(e.EV_KEY, e.KEY_1, 1)
+            cntrl.emit(uinput.BTN_B, 1)
         else:
-            cntrl.write(e.EV_KEY, e.KEY_1, 0)
+            cntrl.emit(uinput.BTN_B, 0)
 
         if cmd[1] == "C:ON":
-            cntrl.write(e.EV_KEY, e.KEY_2, 1)
+            cntrl.emit(uinput.BTN_X, 1)
         else:
-            cntrl.write(e.EV_KEY, e.KEY_2, 0)
+            cntrl.emit(uinput.BTN_X, 0)
 
         if cmd[1] == "D:ON":
-            cntrl.write(e.EV_KEY, e.KEY_3, 1)
+            cntrl.emit(uinput.BTN_Y, 1)
         else:
-            cntrl.write(e.EV_KEY, e.KEY_3, 0)
+            cntrl.emit(uinput.BTN_Y, 0)
 
-        
+
         #if cmd[1] == "L3:ON":
          #   cntrl.write(e.EV_KEY, e.KEY_4, 1)
         #else:
          #   cntrl.write(e.EV_KEY, e.KEY_4, 0)
-        
 
-            
+
+
 
     elif cmd[0] == "JST":
         coord = cmd[1].split(";")
-        cntrl.write(e.EV_ABS, e.ABS_X, coord[0])
-        cntrl.write(e.EV_ABS, e.ABS_Y, coord[1])
+        cntrl.emit(uinput.ABS_X, coord[0])
+        cntrl.emit(uinput.ABS_Y, coord[1])
 
     return cmd
 
@@ -73,6 +78,7 @@ def setup():
 
 
 if __name__ == '__main__':
+    print("Code started\n")
     ser = setup()
     print("Found serial " + ser)
     ser.flush()
@@ -82,3 +88,5 @@ if __name__ == '__main__':
         if ser.in_waiting > 0:
             command = ser.readline().decode('utf-8').rstrip()
             print(decode_command(command, cntrl=controller))
+
+
