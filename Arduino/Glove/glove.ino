65314877f57c5bfb9a3b9383faf2ac920fc0fd5c
mene4485 <meneliknouvellon@gmail.com>
2022-06-02 17:39:06 +0200

    Update glove.ino

--- Arduino/Glove/glove.ino
+++ Arduino/Glove/glove.ino
@@ -1,169 +1,171 @@
+/* Program for the glove controller : 2 flexometers and a gyroscope*/
 
 #include <Arduino_LSM9DS1.h>
 
+int i = 0;
+int j = 0;
+float x, y, z;
+double moy_x, moy_y, moy_z = 0;
+int aux = 0;
 
-  // Constants
-  #define BTN_0 2 // ?? 
+const int flexPin1 = A0;
+const int flexPin2 = A1;
 
-  // Parameters
-  float x_start, y_start, z_start;
+int val1;
+int val2;
 
-
-  //Variables
-  bool configured = false;
-
-
-  uint32_t btn = 0;
-  bool btn1 = false;
-  bool btn2 = false;
-
-
-
-  /**
-   * @brief 
-   * This functions is going to tell us if the levier is up.
-   * We are going to emulate a button thanks to this
-   * 
-   * 
-   * @param analogValue 
-   * @return true if its up 
-   * @return false  if not
-   */
-  bool button1(float y){
-    //&& (z/z_start) < 0.8
-    if((y/y_start) > 1.5 ){
-      Serial.println("droite");
-      return true;
-    }else{
-      return false;}
-  }
-  /**
-   * @brief 
-   *This functions is going to tell us if the levier is down.
-  * We are going to emulate a button thanks to this 
-  * @param analogValue 
-  * @return true if its down
-  * @return false  if not
-  */
-  bool button2(float y){
-    //&& (z/z_start) < 0.6
-    if((y/y_start) < 0){
-      Serial.println("gauche");
-      return true;
-    }else{
-      return false;}
-  }
+uint32_t btn = 0;
+bool btn0 = false;
+bool btn1 = false;
+bool btn2 = false;
+bool btn3 = false;
+int x_pos = 0;
+int x_neg = 0;
 
 
-
-
-  /**
-   * @brief this function setup the arduino
-   * 
-   * @return ** void 
-   */
-  void setup(void) {
-    Serial.begin(9600);
-    while (!Serial);
-    Serial.println("Started");
-  
-    if (!IMU.begin()) {
-      Serial.println("Failed to initialize IMU!");
-      while (1);
-    }
-    Serial.print("Magnetic field sample rate = ");
-    Serial.print(IMU.magneticFieldSampleRate());
-    Serial.println(" Hz");
-    Serial.println();
-    Serial.println("Magnetic Field in uT");
-    Serial.println("X\tY\tZ");
-    if (IMU.magneticFieldAvailable()) {
-      IMU.readMagneticField(x_start, y_start, z_start);
-    }
-    pinMode(BTN_0, INPUT_PULLUP); //??? 
-
-  }
-
-
-
-  void loop(void) {
-    float x, y, z;
-    if (IMU.magneticFieldAvailable()) {
-        IMU.readMagneticField(x, y, z);
-        bool btn1_new = button1(y);
-        bool btn2_new = button2(y);
-    
-        if (btn1 != btn1_new || btn2 != btn2_new) {
-              btn1 = btn1_new;
-              btn2 = btn2_new;
-
-              btn = 0;
-              btn = btn << 1;
-              if (btn1) { btn = btn + 1;}
-              btn = btn << 1;
-              if (btn2) { btn = btn + 1;}
-              btn = btn << 1;
-     
-              btn = btn << 20;
-
-
-              Serial.println(btn);
-      
-              delay(40);
-          }
-    }
-  }
-
-  
-    
-
-/*
-
-float x_start, y_start, z_start;
-  
-
-#include <Arduino_LSM9DS1.h>
+int droite(int i);
+int gauche(int i);
+bool isButton1(int j);
+bool isButton2(int j);
+bool isFlexButton(int j);
+void readGyro();
 
 void setup() {
   Serial.begin(9600);
   while (!Serial);
-  Serial.println("Started");
+  //Serial.println("Started");
 
   if (!IMU.begin()) {
-    Serial.println("Failed to initialize IMU!");
+    //Serial.println("Failed to initialize IMU!");
     while (1);
   }
-  Serial.print("Magnetic field sample rate = ");
-  Serial.print(IMU.magneticFieldSampleRate());
+  /*
+  Serial.print("Gyroscope sample rate = ");
+  Serial.print(IMU.gyroscopeSampleRate());
   Serial.println(" Hz");
   Serial.println();
-  Serial.println("Magnetic Field in uT");
-  Serial.println("X\tY\tZ");
-  if (IMU.magneticFieldAvailable()) {
-    IMU.readMagneticField(x_start, y_start, z_start);
-  }
+  Serial.println("Gyroscope in degrees/second");
+  Serial.println("X\tY\tZ");*/
 }
 
 void loop() {
-  float x, y, z;
+  
 
-  if (IMU.magneticFieldAvailable()) {
-    IMU.readMagneticField(x, y, z);
+  
 
-    Serial.print(x);
-    Serial.print('\t');
-    Serial.print(y);
-    Serial.print('\t');
-    Serial.println(z);
-    //&& (z/z_start) < 0.8
-    if((y/y_start) > 1.5 ){
-      Serial.println("droite");
+  readGyro();
+   
+  val1 = analogRead(flexPin1);
+  val2 = analogRead(flexPin2);
+
+  /*Serial.print("flex 1 = ");
+  Serial.print('\t');
+  Serial.print(val1);
+  Serial.print('\t');
+  Serial.print(", flex 2 = ");
+  Serial.print('\t');
+  Serial.println(val2);*/
+  
+  bool btn0_new = isButton1(j);
+  bool btn1_new = isButton2(j);
+  bool btn2_new = isFlexButton(val1);
+  bool btn3_new = isFlexButton(val2);
+  int x_pos_new = getXValue(i);
+
+  if (btn0 != btn0_new || btn1 != btn1_new || btn2 != btn2_new  || btn3 != btn3_new || x_pos_new != 
+  x_pos){
+    
+  
+    btn0 = btn0_new;
+    btn1 = btn1_new;
+    btn2 = btn2_new;
+    btn3 = btn3_new;
+    x_pos = x_pos_new;
+    btn = 0;
+    if (btn2) {btn = btn + 1;}
+    btn = btn << 1;
+    if (btn3) { btn = btn + 1;}
+    btn = btn << 1;
+    if (btn0) { btn = btn + 1;}
+    btn = btn << 1;
+    if(btn1){btn+=1;}
+
+    uint32_t cmd = btn;
+    cmd = cmd << 10;
+    cmd = cmd + 512;
+    cmd = cmd << 10;
+    cmd = cmd + x_pos;
+    Serial.println(cmd);
+  }
+  
+  
+  delay(100);
+}
+
+int getXValue(int a){
+  if(a <= -1){
+    //Serial.println(a);
+    //Serial.println("gauche");
+    i = -1;
+    return 0;
+  }else if(a >= 1){
+    i = 1;
+    return 1022;
+  }
+  //Serial.println("normal");
+
+  //Serial.println(a);
+  return 512;
+}
+
+bool isButton1(int a){
+  if(a >= 1){
+    //Serial.println(a);
+    j = 1;
+    //Serial.println("button 1");
+    return true;
+  }
+  return false;
+}
+
+bool isButton2(int a){
+  if(a <= -1){
+    //Serial.println(a);
+    j = -1;
+    //Serial.println("button 2");
+    return true;
+  }
+  return false;
+}
+
+bool isFlexButton(int a){
+  if(a < 250){
+    //Serial.println("Je te flex dessus");
+    //Serial.println(a);
+    return true;
+  }
+  return false;
+}
+
+void readGyro(){
+  if (IMU.gyroscopeAvailable()) {
+    IMU.readGyroscope(x, y, z);
+    
+    if(x>200){
+        i += 1;
+        x=0;
+    }else if(x < -200){
+        i -= 1;
+        x=0;
     }
-     //&& (z/z_start) < 0.6
-    if((y/y_start) < 0){
-      Serial.println("gauche");
+
+    if(y>200){
+        j += 1;
+        y=0;
+    }else if(y < -200){
+        j -= 1;
+        y=0;
     }
     
   }
-  delay(500);
-}*/
+}
